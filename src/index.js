14e706961833a7c41b190125c7f7b44a9dd221b7
Abhay Sharma <110305361+AbhaySharma-3014@users.noreply.github.com>
2024-07-08 00:51:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,17 +9,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 const url = 'https://dailyjournal-qxeq.onrender.com/api/posts'; // Replace with your Render URL
 const interval = 30000; // Interval in milliseconds (30 seconds)
 
-function reloadWebsite() {
-axios.get(url)
-  .then(response => {
-    console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
-  })
-  .catch(error => {
-    console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
-  });
-}
-
-setInterval(reloadWebsite, interval);
 root.render(
     <App />
 );
